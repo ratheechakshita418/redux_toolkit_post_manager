db68f108c4fde4b0bbc85b34d78bd298795170f3
ratheechakshita418 <ratheechakshita418@gmail.com>
2026-07-21 11:49:32 +0530

    Add Navbar component for application header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+      <h1>Redux Toolkit Post Manager</h1>
+      <p>Centralized State Management using Redux Toolkit</p>
+    </header>
+  );
+}
+
+export default Navbar;
